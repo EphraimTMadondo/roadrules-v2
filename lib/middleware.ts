e3cb6c23bff261c3f150e5de721a7efc4d60c2e0
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-02-12 16:10:22 +0200

    removed session env vars, session middleware log and post request

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -6,7 +6,7 @@ export function logRequest ( req: NextApiRequest, res: NextApiResponse ) {
   console.log( "\nURL:", req.url );
   console.log( "\nMETHOD:", req.method );
   console.log( "\nHeaders:", JSON.stringify( req.headers ) );
-  console.log( "\nSession:", JSON.stringify( req.session ) );
+  // console.log( "\nSession:", JSON.stringify( req.session ) );
 
   console.log( "\n\nBody:", JSON.stringify( req.body ) );
 
