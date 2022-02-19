81fa141ea9ed08c15f489d35fcb53ee008bf9001
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-02-19 15:40:42 +0200

    refactored notes page, removed old verify page

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,79 +1,61 @@
-export const a = "zxc";// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import Button from '@mui/material/Button';
-// import Head from 'next/head';
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { CustomCenter } from '../components/custom-center';
-// import Layout from '../components/layout';
-// import { UNIT } from '../lib/units';
-// import Typography from '@mui/material/Typography';
+import Head from 'next/head';
+import { CustomCenter } from '../components/custom-center';
+import Layout from '../components/layout';
 
-// export default function Notes () {
+export default function Notes () {
 
-//   const notes = [
-//     "ZIMBABWE'S ROAD RULES",
-//     "TEST SYLLABUS",
-//     "LEARNER DRIVER'S LICENSE TEST",
-//     "DRIVER'S LICENSE CLASSES",
-//     "VEHICLE CLASSES",
-//     "VEHICLE DOCUMENTATION",
-//     "PERSONS WITH DIASABILITIES",
-//     "REQUIRED DOCUMENTATION",
-//     "SPEED LIMITS",
-//     "NIGHT DRIVING",
-//     "OVERTAKING",
-//     "DRIVING ON AN UPHILL OR DOWNHILL",
-//     "ROAD LANE USE",
-//     "SPECIAL VEHICLES",
-//     "INTRODUCTION TO DRIVING",
-//     "TRAFFIC SIGNS & SIGNALS",
-//     "HAZARDOUS CONDITIONS"
-//   ];
+  const notes = [
+    "ZIMBABWE'S ROAD RULES",
+    "TEST SYLLABUS",
+    "LEARNER DRIVER'S LICENSE TEST",
+    "DRIVER'S LICENSE CLASSES",
+    "VEHICLE CLASSES",
+    "VEHICLE DOCUMENTATION",
+    "PERSONS WITH DIASABILITIES",
+    "REQUIRED DOCUMENTATION",
+    "SPEED LIMITS",
+    "NIGHT DRIVING",
+    "OVERTAKING",
+    "DRIVING ON AN UPHILL OR DOWNHILL",
+    "ROAD LANE USE",
+    "SPECIAL VEHICLES",
+    "INTRODUCTION TO DRIVING",
+    "TRAFFIC SIGNS & SIGNALS",
+    "HAZARDOUS CONDITIONS"
+  ];
 
-//   return (
-//     <Layout>
+  return (
+    <Layout>
 
-//       <Head>
-//         <title>Road Rules - Notes</title>
-//       </Head>
+      <Head>
+        <title>Road Rules - Notes</title>
+      </Head>
 
-//       <div className="row center-center">
-//         <CustomCenter>
-//           <div className="row start-center">
-//             <span style={{ fontWeight: "bold", fontSize: "1.2em", padding: UNIT * 2 }}>
-//               Notes
-//             </span>
-//           </div>
-//         </CustomCenter>
-//       </div>
+      <div className="row center-center">
+        <CustomCenter>
+          <div className="row-wrap start-stretch">
+            {
+              notes
+                .map( ( note, index ) => (
+                  <div
+                    key={index}
+                    className="column start-stretch gt-sm-50"
+                    style={{ height: "auto", padding: 8 * 2 }}
+                  >
+                    <div
+                      className="card link pointed column start-start"
+                      style={{ height: "100%", padding: 8 }}>
+                      <span style={{ fontSize: "1.5em" }}>
+                        {note}
+                      </span>
+                    </div>
+                  </div>
+                ) )
+            }
+          </div>
+        </CustomCenter>
+      </div>
 
-//       <div className="row center-center">
-//         <CustomCenter>
-//           <div className="row-wrap start-stretch">
-//             {
-//               notes
-//                 .map( ( note, index ) => (
-//                   <div
-//                     key={index}
-//                     className="column start-stretch gt-sm-50"
-//                     style={{ height: "auto", padding: UNIT * 2 }}
-//                   >
-//                     <div
-//                       className="card link pointed column start-start"
-//                       style={{ height: "100%", padding: UNIT }}>
-//                       <span style={{ fontSize: "1.5em" }}>
-//                         {note}
-//                       </span>
-//                     </div>
-//                   </div>
-//                 ) )
-//             }
-//           </div>
-//         </CustomCenter>
-//       </div>
-
-//     </Layout>
-//   )
-// }
+    </Layout>
+  )
+}
