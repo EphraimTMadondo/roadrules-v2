9ab5c313659dfdfa91e47b3bc0205a9f05dd3f03
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-04-21 08:20:00 +0200

    remove exclamations and add spacing below wrong

--- components/wrong-alert.tsx
+++ components/wrong-alert.tsx
@@ -12,9 +12,9 @@ export function WrongAlert(props: Props) {
   const { question, selectedOption } = props;
   return (
     <div className="flex flex-col justify-center items-stretch my-4 bg-red-100/50 rounded">
-      <div className="flex flex-row justify-center items-center pt-2 px-4">
+      <div className="flex flex-row justify-center items-center pt-2 pb-2 px-4">
         <span className="text-lg font-semibold pr-1 text-center text-red-700">
-          Wrong!
+          Wrong
         </span>
       </div>
       <div className="flex flex-row justify-center items-center px-4">
