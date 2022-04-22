14db1a5937d95e54c3096b6ea2bc2b71d28eb671
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-04-22 11:00:26 +0200

    move montserrat font back to _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
+        {/* <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap"
-        />
+        /> */}
       </Head>
       <body className="bg-slate-100">
         <Main />
