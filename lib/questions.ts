693517a0132fcd51c116e8be46c756bfd14b6298
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-05-17 13:00:30 +0200

    Feat: Add questions sorting according to paid status
    Will only pick the first LIMIT questions by id if unpaid,
    will randomize if paid

--- lib/questions.ts
+++ lib/questions.ts
@@ -1,10 +1,16 @@
 import { prisma } from './db';
 
-export async function getQuestions(limit: number) {
+export async function getQuestions(limit: number, paid: boolean) {
   const questions = await prisma.question.findMany();
 
   const randomQuestions = questions
-    .sort(() => Math.random() - 0.5)
+    .sort((a, b) => {
+      if (paid) {
+        return Math.random() - 0.5;
+      }
+
+      return a.id - b.id;
+    })
     .splice(0, limit);
 
   return randomQuestions;
