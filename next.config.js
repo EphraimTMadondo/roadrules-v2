2e90866da4efeeaf6097be83ea40aced47a58a60
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-06-21 21:02:13 +0200

    Chore: Add redirection config

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = withPWA({
     runtimeCaching,
     disable: process.env.NODE_ENV === "development"
   },
+  redirects: async () => [
+    {
+      source: '/:path*',
+      has: [{ type: 'host', value: 'roadrules.co.zw' }],
+      destination: 'https://www.roadrules.co.zw/:path*',
+      permanent: true
+    }
+  ]
 })
